a869d135ad46cdfcbd1537eea9ce9b46ccd36821
Alex Lementuev <a.lementuev@gmail.com>
2016-01-30 18:00:56 -0800

    Refactoring

--- PixelSpaceOdyssey/enemy_base.h
+++ PixelSpaceOdyssey/enemy_base.h
@@ -14,52 +14,59 @@ extern long random(long, long);
 void EnemyUpdate(Character *character, TimeInterval dt);
 void UpdateConstraints(Character *character);
 
+/** The farthest point enemy can see on the left */
 inline int16_t EnemyGetMinSightX(const Character *self)
 {
     int16_t sightX = self->x - (self->dir == DIR_LEFT ? self->sightDistanceForward : self->sightDistanceBackward);
     return sightX < self->sightMinX ? self->sightMinX : sightX;
 }
 
+/** The farthest point enemy can see on the right */
 inline int16_t EnemyGetMaxSightX(const Character *self)
 {
     int16_t sightX = self->x + (self->dir == DIR_RIGHT ? self->sightDistanceForward : self->sightDistanceBackward);
     return sightX > self->sightMaxX ? self->sightMaxX : sightX;
 }
 
+/** True if there's no obstacle on the enemy's left */
 inline bool EnemyCanMoveLeft(const Character *self)
 {
     return CharacterGetLeft(self) > self->moveMinX;
 }
 
+/** True if there's no obstacle on the enemy's right */
 inline bool EnemyCanMoveRight(const Character *self)
 {
     return CharacterGetRight(self) < self->moveMaxX;
 }
 
+/** True if enemy can move into the direction it's currently facing */
 inline bool EnemyCanMove(const Character *self)
 {
     return (self->dir == DIR_LEFT  && EnemyCanMoveLeft(self)) ||
            (self->dir == DIR_RIGHT && EnemyCanMoveRight(self));
 }
 
-/// Time elasped since last attack is enough for a new attack
+/** True if enought time passed since the last enemy's attack  */
 inline bool EnemyCanAttack(const Character *self)
 {
     return millis() - self->lastAttackTimestamp > ENEMY_ATTACK_DELAY;
 }
 
-/// Enemy is close enought to the last player's position to attack
+/** Enemy is close enought to the last player's position to attack */
 inline bool EnemyIsCloseToAttack(const Character *self)
 {
     return ABS(self->lastPlayerX - self->x) <= DIV2(self->width + player.width);
 }
 
+/** Initiates an attack to the player */
 inline void EnemyAttack(Character *self)
 {
     self->lastAttackTimestamp = millis();
     playerDamage(self);
 }
 
+/** Set enemy's target position */
 inline void EnemySetTargetPos(Character *self, int16_t target)
 {
     self->targetPos = target;
